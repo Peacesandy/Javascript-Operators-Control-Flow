6b2f0470fb76c28ea66da532c2b24ca2982f1a85
Sandy <peacesandy04@gmail.com>
2023-02-27 12:28:45 -0800

    Added js solution

---
+++ index.js
@@ -0,0 +1,13 @@
+let Bemi = 2
+
+if (Bemi > 1) {
+    console.log('Government, Economics, Literature, History, English, Mathematics')
+}else {
+    console.log(' Physics, Chemistry, Biology, Technical Drawing ')
+}
+
+
+for(let i = 1; i < 20; i += 7) {
+    console.log(i)
+}
+
